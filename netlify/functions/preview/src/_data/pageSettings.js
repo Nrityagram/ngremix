901508397b9548017f703b86685a8ff826fa8000
Amrita Chanda <amrita.chanda@gmail.com>
2024-05-27 20:36:02 +0530

    Update: only generating webp images

--- netlify/functions/preview/src/_data/pageSettings.js
+++
@@ -1,8 +0,0 @@
-const settings = new Object()
-
-settings.sliderOnPage = false
-settings.gismapOnPage = false
-settings.youtubeOnPage = false
-settings.marqueeOnPage = false
-
-module.exports.pageSettings = settings
